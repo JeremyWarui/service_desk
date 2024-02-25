7c0a2496132ca863eba5f1de608d827c7025e95d
JeremyWarui <jerywarne7@gmail.com>
2024-02-25 22:19:34 +0300

    Deploy to vercel

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,24 @@
-import express from "express";
-import dotenv from "dotenv";
-import router from "./routes/index";
-import cors from "cors";
+import express from 'express';
+import dotenv from 'dotenv';
+import router from './routes/index';
+import cors from 'cors';
 
 dotenv.config();
 
-export const app = express();
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    // origin that is allowed to access the api
-    origin: "http://localhost:3000",
-  })
-);
-app.use("/", router);
+app.use(express.urlencoded({extended: true}));
+app.use(cors({
+  // origin that is allowed to access the api
+  origin: 'http://localhost:3000'
+}));
+app.use('/', router);
 
 app.listen(port, () => {
-  // console.log('Server started successfully');
-  console.log(`Server running on  ${port}, 1:${port}`);
+    // console.log('Server started successfully');
+    console.log(`Server running on  ${port}, 1:${port}`)
 });
+
+export default app;
